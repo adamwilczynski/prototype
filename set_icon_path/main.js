464419ac243e433dcaff316f1696184bed0be626
Adam <adam.wilcz7@gmail.com>
2022-06-26 04:51:17 +0200

    Optimization

--- set_icon_path/main.js
+++
@@ -1,9 +0,0 @@
-// Icons derived from Emoji One BW icons
-
-'use strict';
-
-var currentUrl = window.location.hostname.toLowerCase();
-
-console.log(currentUrl);
-// chrome.browserAction.setIcon({path: 'icon2.png'});
-// chrome.browserAction.setIcon({path: 'icon2.png'});
